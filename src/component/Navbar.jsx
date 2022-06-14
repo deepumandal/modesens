8eba99342cbf63804a7a3dd0d433096c6c368bca
DEEPAK KUMAR MANDAL <nomercyhunters@gmail.com>
2022-06-14 15:25:25 +0530

    navbar updated /// pending

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import utility from "../styles.module/utility.module.css";
 import style from "../styles.module/Navbar.module.css";
 import cx from "classnames";
-import { useDispatch , useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getnavdataapi } from "../store/Navbar/nav.action";
+import Navdropdown from "./Navdropdown";
 
 const Navbar = () => {
   // let Navbar = [
@@ -17,18 +18,38 @@ const Navbar = () => {
   //   "COMMUNITY",
   //   "WHY MODESENS",
   // ];
-// navbar api fetch here
-const dispatch = useDispatch()
-const {loading,error, navdata : data} = useSelector((state)=> state.navbar)
+  // navbar api fetch here
+  const dispatch = useDispatch();
+  const {
+    loading,
+    error,
+    navdata: data,
+  } = useSelector((state) => state.navbar);
   useEffect(() => {
-    dispatch(
-      getnavdataapi()
-    )
-  }, [])
-  
+    dispatch(getnavdataapi());
+  }, []);
 
+  // mousehover event on subcategory
+  const [selected, setselected] = useState([]);
 
-  
+  const someonehoverme = (e) => {
+    const name = e.target.innerHTML;
+    const item = data.find(isfind);
+
+    function isfind(i) {   
+      
+      if (i.categoryname === name.trim()) {
+        setselected(i);
+        // console.log(i)
+      }
+
+      // console.log(selected)
+    }
+
+    
+  };
+  // ends here
+
   return (
     <div>
       {/* <div ><NavTopbar /></div> */}
@@ -36,36 +57,44 @@ const {loading,error, navdata : data} = useSelector((state)=> state.navbar)
         <div className={cx(utility.container)}>
           <div className={style.navbar}>
             <div id={style.leftdiv}>
-              <img className={utility.cursurhover}
+              <img
+                className={utility.cursurhover}
                 id={style.logo}
                 src="https://cdn.modesens.com/static/img/20190228newlogo-black.svg"
                 alt=""
               />
 
               <div className={cx(utility.flex)}>
-                {data.map((Categ, index) => (  // console.log(Categ.sub.categoryname)
-                  <div key={index} className={cx(style.subcateg, utility.cursurhover) }>
-                    {" "}
-                    {Categ.sub.categoryname}{" "}
-                  </div>
-                  
-                ))}
+                {data.map(
+                  (
+                    Categ,
+                    index // console.log(Categ.sub.categoryname)
+                  ) => (
+                    <div
+                      key={index}
+                      name={Categ.categoryname}
+                      className={cx(style.subcateg, utility.cursurhover)}
+                      onMouseOver={someonehoverme}
+                    >
+                      {" "}
+                      {Categ.categoryname}{" "}
+                    </div>
+                  )
+                )}
               </div>
             </div>
 
             {/* login userid countary starts here */}
             <div id={style.rightdiv}>
               flag
-
               {/* having a seperate component here */}
-
               <img
-               className={cx(style.subcateg, utility.cursurhover) }
+                className={cx(style.rightsubcateg, utility.cursurhover)}
                 src="https://cdn.modesens.com/static/img/20200612shopping_bag2.svg"
                 alt=""
               />
               <img
-               className={cx(style.subcateg, utility.cursurhover) }
+                className={cx(style.rightsubcateg, utility.cursurhover)}
                 src="https://cdn.modesens.com/static/img/20200612account_b2.svg"
                 alt=""
               />
@@ -75,16 +104,28 @@ const {loading,error, navdata : data} = useSelector((state)=> state.navbar)
                   alignItems: "center",
                   padding: "2px",
                 }}
-                className={utility.cursurhover}              >
+                className={utility.cursurhover}
+              >
                 <img
                   src="https://cdn.modesens.com/static/img/20200612search_img1.svg"
                   alt=""
                 />
 
-                <span style={{ marginLeft: "5px" }}> SEARCH</span>
+                <span
+                  style={{
+                    marginLeft: "5px",
+                    fontWeight: 500,
+                    letterSpacing: 0.2,
+                  }}
+                >
+                  {" "}
+                  SEARCH
+                </span>
               </span>
             </div>
           </div>
+
+          <Navdropdown props={selected} disi = {true} />
         </div>
       </div>
     </div>
