3bc4389f6fb0eb00fb2a4f4a496cc742b8b7b300
PrathyushNair <prathyu360@gmail.com>
2022-06-16 20:27:23 +0530

    test

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
+import { authReducer } from "./Login/auth.reducer";
 import { mensreducer } from "./Mens/mensreducer";
 import { navReducer } from "./Navbar/navReducer";
 
@@ -7,7 +8,7 @@ import { navReducer } from "./Navbar/navReducer";
 const rootReducer = combineReducers({
     navbar: navReducer,
     mens:mensreducer,
-    // auth:authReducer
+    auth:authReducer
 })
 
 
