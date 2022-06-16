a4b0ea990640ee74901cafb399c60ad268e7833b
DEEPAK KUMAR MANDAL <nomercyhunters@gmail.com>
2022-06-16 16:22:00 +0530

    bottom area added

---
+++ src/pages/Bottom.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Bottom = () => {
+  return (
+    <div className='container-fluid' >
+        <div className='container m5' style={{ 
+        backgroundColor: "#f8f7f6",
+        width:"1200px",
+        margin : "auto",
+        height : "800px"
+
+        }} >
+            dsafjk
+        </div>
+    </div>
+  )
+}
+
+export default Bottom
