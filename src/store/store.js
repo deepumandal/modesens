4b463144785aea249ee14a46c16d87c7d5a46ad4
PrathyushNair <prathyu360@gmail.com>
2022-06-16 20:33:59 +0530

    adding addreducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,16 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
+import { authReducer } from "./Login/auth.reducer";
 
 import { mensreducer } from "./Mens/mensreducer";
 import { navReducer } from "./Navbar/navReducer";
-import authReducer from './Login/auth.reducer'
+
 
 
 const rootReducer = combineReducers({
     navbar: navReducer,
     mens:mensreducer,
-    // auth:authReducer
+    auth:authReducer
 })
 
 
