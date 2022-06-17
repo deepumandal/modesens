1072a531f72a72e2c229c4aee04d591da1c413d4
GaganChandan2003 <101565932+GaganChandan2003@users.noreply.github.com>
2022-06-17 12:46:50 +0530

    done

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,6 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import { authReducer } from "./Login/auth.reducer";
-
 import { mensreducer } from "./Mens/mensreducer";
 import { navReducer } from "./Navbar/navReducer";
 
